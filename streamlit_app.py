a1b2010b21785dbae0abab4262f9311fe714c5d1
prasadraju9999 <52656634+prasadraju9999@users.noreply.github.com>
2022-09-14 16:41:44 +0530

    Update streamlit_app.py
    added list with pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My wifes new hotel")
 streamlit.header("Breakfast Menu")
@@ -9,3 +10,5 @@ streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}
 streamlit.text('\N{mechanical arm}')
 
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
