d7545deb81c639b4a77a1af69f5ad562a3bcbbe5
prasadraju9999 <52656634+prasadraju9999@users.noreply.github.com>
2022-09-14 16:38:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.header("Breakfast Menu")
 streamlit.text("idly")
 streamlit.text("dosa")
 streamlit.text("vada")
-streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+
 
