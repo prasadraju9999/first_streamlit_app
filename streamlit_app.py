d7f75abd1d6d3a4097a853486dfe5c16157547ca
prasadraju9999 <52656634+prasadraju9999@users.noreply.github.com>
2022-09-14 16:43:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text("dosa")
 streamlit.text("vada")
 streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 streamlit.text('\N{mechanical arm}')
+streamlit.header('\N{call me hand} Find the list below')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
